ddbb4d468f8811971b655cd3adb1721d67b48032
Kennedy <santes1263@gmail.com>
2024-08-25 16:55:03 +0300

    Add main packages and index.js file (with test database)

---
+++ src/index.mjs
@@ -0,0 +1,52 @@
+import express from 'express'
+import { json } from 'express'
+import mongoose from 'mongoose'
+import REQUEST_VIEWER from './utils/customMiddlewares/request_viewer.mjs'
+import productRouter from './routers/productRouter.mjs'
+import passport from 'passport'
+import cookieParser from 'cookie-parser'
+import authRouter from './routers/authRouter.mjs'
+import session  from 'express-session'
+import usersRouter from './routers/usersRouter.mjs'
+
+const app = express()
+
+
+//Middlewares
+app.use(json())
+app.use(REQUEST_VIEWER)
+app.use(session({
+  secret: 'dev',
+  saveUninitialized: false,
+  resave: false,
+  cookie: {maxAge: 60000 * 3}
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//Routes
+app.use(productRouter)
+app.use(authRouter)
+app.use(usersRouter)
+
+
+
+
+app.get('/', (req, res) => {
+    res.status(200).send("hello")
+})
+
+
+
+const PORT = process.env.PORT || 3000;
+
+mongoose
+.connect("mongodb://localhost:27017/change_it")
+.then(() => console.log("Connected"))
+.catch((err) => console.log(err));
+
+
+app.listen(PORT, () => {
+  console.log(`Running on ${PORT}`);
+});
