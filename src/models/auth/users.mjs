52c15d7f23a95ebc09694ea805e42898ea540888
Kennedy <santes1263@gmail.com>
2024-08-25 16:57:20 +0300

    Create models

---
+++ src/models/auth/users.mjs
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+
+const UserSchema = new Schema({
+    username: {type: String, required: true},
+    password: {type: String, required: true}
+})
+
+const UserModel = mongoose.model('UserModel', UserSchema)
+
+export default UserModel
