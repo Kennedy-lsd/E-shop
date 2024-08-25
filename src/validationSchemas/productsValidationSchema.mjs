29de3caa70463a1590ce0c9b7e18f87b6a15e1db
Kennedy <santes1263@gmail.com>
2024-08-25 21:14:39 +0300

    Update validator

---
+++ src/validationSchemas/productsValidationSchema.mjs
@@ -0,0 +1,64 @@
+export const createProductValidation = {
+    image: {
+        isString: true,
+        notEmpty: {
+            errorMessage: "Required image"
+        }
+    },
+    title: {
+        isString: {
+            errorMessage: "Title must be a string"
+        },
+        notEmpty: {
+            errorMessage: "Title can't be empty"
+        }
+    },
+    rating: {
+        isLength: {
+            min: 0,
+            max: 5,
+        },
+        notEmpty: {
+            errorMessage: "Required rating"
+        },
+        isDecimal: {
+            errorMessage: "Rating must be a number"
+        }
+    },
+    colorway: {
+        isString: {
+            errorMessage: "Color must be a string"
+        },
+        notEmpty: {
+            errorMessage: "Color can't be empty"
+        }
+    },
+    brand: {
+        isString: {
+            errorMessage: "Brand must be a string"
+        },
+        notEmpty: {
+            errorMessage: "Brand can't be empty"
+        }
+    },
+    model: {
+        isString: {
+            errorMessage: "Model must be a string"
+        },
+        notEmpty: {
+            errorMessage: "Model can't be empty"
+        }
+    },
+    code: {
+        isString: {
+            errorMessage: "Code must be a string"
+        },
+        notEmpty: {
+            errorMessage: "Code can't be empty"
+        },
+        isLength: ({
+            min: 6,
+            max: 6
+    })
+    }
+}
