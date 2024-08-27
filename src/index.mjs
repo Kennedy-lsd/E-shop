e7d07bef20692804a7b13e51f53c3354361d6ec6
Kennedy <santes1263@gmail.com>
2024-08-27 16:45:15 +0300

    Add cors, dotenv Middlewares and update structure

--- src/index.mjs
+++ src/index.mjs
@@ -1,50 +1,61 @@
-import express from 'express'
-import { json } from 'express'
-import mongoose from 'mongoose'
-import REQUEST_VIEWER from './utils/customMiddlewares/request_viewer.mjs'
-import productRouter from './routers/productRouter.mjs'
-import passport from 'passport'
-import cookieParser from 'cookie-parser'
-import authRouter from './routers/authRouter.mjs'
-import session  from 'express-session'
-import usersRouter from './routers/usersRouter.mjs'
+import express from "express";
+import { json } from "express";
+import mongoose from "mongoose";
+import REQUEST_VIEWER from "./utils/customMiddlewares/request_viewer.mjs";
+import productRouter from "./routers/productRouter.mjs";
+import GoogleAuthRouter from "./routers/googleAuthRouter.mjs";
+import passport from "passport";
+import cookieParser from "cookie-parser";
+import authRouter from "./routers/authRouter.mjs";
+import session from "express-session";
+import usersRouter from "./routers/usersRouter.mjs";
+import dotenv from "dotenv";
+import cors from "cors";
+import MongoStore from "connect-mongo";
+import "./authStrategy/google.mjs";
 
-const app = express()
+const app = express();
+
+dotenv.config();
+
 
 
 //Middlewares
-app.use(json())
-app.use(REQUEST_VIEWER)
-app.use(session({
-  secret: 'dev',
-  saveUninitialized: false,
-  resave: false,
-  cookie: {maxAge: 60000 * 3}
-}))
+app.use(json());
+app.use(REQUEST_VIEWER);
+app.use(cors());
+app.use(
+  session({
+    secret: "dev",
+    saveUninitialized: true,
+    resave: false,
+    cookie: { maxAge: 6000 * 60 },
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URL,
+    }),
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 //Routes
-app.use(productRouter)
-app.use(authRouter)
-app.use(usersRouter)
+app.use(productRouter);
+app.use(authRouter);
+app.use(usersRouter);
+app.use(GoogleAuthRouter);
 
-
-
-
-app.get('/', (req, res) => {
-    res.status(200).send("hello")
-})
-
+app.get("/", (req, res) => {
+  res.status(200).send("hello");
+});
 
 const PORT = process.env.PORT || 3000;
 
 mongoose
-.connect("mongodb+srv://Admin:o9kRC99EwBjK0gqT@cluster-proj.1m91v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-proj")
-.then(() => console.log("Connected"))
-.catch((err) => console.log(err));
-
+  .connect(
+    process.env.MONGO_URL
+  )
+  .then(() => console.log("Connected"))
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () => {
   console.log(`Running on ${PORT}`);
