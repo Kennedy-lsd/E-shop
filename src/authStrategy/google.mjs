e9ee56314febef2e3adcc2ecd3a61aa48cdc8141
Kennedy <santes1263@gmail.com>
2024-08-28 01:02:34 +0300

    Update google Oauth

--- src/authStrategy/google.mjs
+++ src/authStrategy/google.mjs
@@ -12,44 +12,47 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser(async (id, done) => {
-	console.log("Inside google deserializer")
-	try {
-		const findUser = await GoogleModel.findById(id)
-		return findUser ? done(null, findUser) : done(null, null)
-	} catch (error) {
-		done(error, null)
-	}
-}) 
+  console.log("Inside google deserializer");
+  try {
+    const findUser = await GoogleModel.findById(id);
+    return findUser ? done(null, findUser) : done(null, null);
+  } catch (error) {
+    done(error, null);
+  }
+});
 
 export default passport.use(
-	new Strategy(
-		{
-			clientID: process.env.GOOGLE_CLIENT_ID,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			callbackURL: "http://localhost:8000/api/auth/google/callback",
-            scope: ['profile', 'email']
-		},
-		async (accessToken, refreshToken, profile, done) => {
-            console.log(profile)
+  new Strategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:8000/api/auth/google/callback",
+      scope: ["profile", "email"],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      console.log(profile);
 
-			let findUser;
-			try {
-				findUser = await GoogleModel.findOne({ email: profile["emails"][0].value });
-			} catch (err) {
-				return done(err, null);
-			}
-			try {
-				if (!findUser) {
-					const newUser = new GoogleModel({
-						email: profile["emails"][0].value
-					});
-					const newSavedUser = await newUser.save();
-					return done(null, newSavedUser);
-				}
-				return done(null, findUser);
-			} catch (err) {
-				return done(err, null);
-			}
-		}
-	)
+      let findUser;
+      try {
+        findUser = await GoogleModel.findOne({
+          email: profile["emails"][0].value
+        });
+      } catch (err) {
+        return done(err, null);
+      }
+      try {
+        if (!findUser) {
+          const newUser = new GoogleModel({
+            email: profile["emails"][0].value,
+			username: profile["name"]["givenName"],
+          });
+          const newSavedUser = await newUser.save();
+          return done(null, newSavedUser);
+        }
+        return done(null, findUser);
+      } catch (err) {
+        return done(err, null);
+      }
+    }
+  )
 );
