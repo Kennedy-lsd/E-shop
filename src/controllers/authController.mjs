e0bdd914e0c46aedff2d8539ae655ece5e23a0db
Kennedy <santes1263@gmail.com>
2024-08-28 01:04:07 +0300

    Update JWT Auth

--- src/controllers/authController.mjs
+++ src/controllers/authController.mjs
@@ -1,25 +1,8 @@
-import pass from "../authStrategy/local.mjs";
-import passport from "passport";
 import UserModel from "../models/auth/usersModel.mjs";
+import {hashPassword, comparePassword} from "../utils/passwordHash.mjs/crypt.mjs";
+import { matchedData, validationResult } from "express-validator";
+import jwt from 'jsonwebtoken'
 
-passport.serializeUser((user, done) => {
-  console.log("Inside serializer");
-  
-  done(null, user.id)
-})
-
-
-passport.deserializeUser(async (id, done) => {    
-  try {
-      console.log(`Inside desirealizer with ${id} id`);
-
-      const findUser = await UserModel.findById(id)
-      if (!findUser) throw new Error("User not found")
-      done(null, findUser)
-  } catch (error) {
-      done(error, null)
-  }
-})
 
 const getStatus = async (req, res) => {
   return req.user
@@ -27,12 +10,64 @@ const getStatus = async (req, res) => {
     : res.status(401).send("Not authorized");
 };
 
-const logOut = async (req, res) => {
-  if (!request.user) return response.sendStatus(401);
-  request.logout((err) => {
-    if (err) return response.sendStatus(400);
-    response.send(200);
-  });
+
+
+const registerUser = async (req, res) => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) return res.status(400).send(result.array());
+
+  const data = matchedData(req);
+
+  console.log('Matched data:', data);
+  try {
+    const existingUser = await UserModel.findOne({ email: data.email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Email already exists' });
+    }
+
+    // Hash the password
+    data.password = await hashPassword(data.password);
+
+    await UserModel.create(data);
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
 };
 
-export { getStatus, logOut };
+
+
+
+const authenticateUser = async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    // Compare passwords
+    if (!comparePassword(req.body.password, user.password)) throw new Error("inccorect password")
+
+    // Generate JWT token
+    const token = jwt.sign({ email: user.email }, 'secret');
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+
+}
+
+const getUser = (async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.user.email });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json({ username: user.username, email: user.email });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+export {authenticateUser, registerUser, getUser, getStatus}
--- src/routers/authRouter.mjs
+++ src/routers/authRouter.mjs
@@ -1,18 +1,17 @@
-import passport from "passport";
-import { Router } from "express";
-import { getStatus, logOut } from "../controllers/authController.mjs";
-const router = Router();
+import {authenticateUser, registerUser, getUser, getStatus} from '../controllers/authController.mjs'
+import { checkSchema } from 'express-validator'
+import { Router } from 'express'
+import verifyToken from "../utils/customMiddlewares/jwt_middleware.mjs";
+import { createUserValidationSchema } from '../validationSchemas/usersValidatorSchema.mjs'
 
-router.post(
-  "/api/auth",
-  passport.authenticate("local"),
-  (request, response) => {
-    response.sendStatus(200);
-  }
-);
+const router = Router()
 
-router.get("/api/auth/status", getStatus);
+router.get('/api/auth/google/status', getStatus)
 
-router.post("/api/auth/logout", logOut);
+router.get('/api/auth/local/status',verifyToken , getUser)
+
+router.post('/api/auth/reg', checkSchema(createUserValidationSchema), registerUser)
+
+router.post('/api/auth/auth', authenticateUser)
 
 export default router
