39ea603a1e80e65d073df37c232670c07ddcd9bb
Kennedy <santes1263@gmail.com>
2024-08-30 18:13:31 +0300

    Create userController tests

--- test/userController.test.mjs
+++ test/userController.test.mjs
@@ -4,7 +4,8 @@ import express from 'express';
 import sinon from 'sinon';
 import routes from '../src/routers/usersRouter.mjs';
 import UserModel from '../src/models/auth/usersModel.mjs'; // adjust the path as necessary
-
+import { redisClient } from "../src/utils/db/redisSetUp.mjs";
+import * as hashUtils from '../src/utils/passwordHash.mjs/crypt.mjs'
 describe('GET /api/users', () => {
     let app;
 
@@ -51,3 +52,95 @@ describe('GET /api/users', () => {
         expect(response.body).to.have.property('message', 'Database error');
     });
 });
+
+describe('GET /api/users/:id', () => {
+    let app;
+    
+    before(() => {
+        app = express()
+        app.use('/', routes)
+    }),
+
+    afterEach(() => {
+        sinon.restore()
+    })
+
+    it('should return a 200 status and an userObject', async () => {
+        const mockUser = { _id: '507f1f77bcf86cd799439011', email: 'exemple@.gmail.com', username: 'Asan', password: '123' };
+        sinon.stub(UserModel, 'findById').returns(Promise.resolve(mockUser))
+        sinon.stub(redisClient, 'get').returns(Promise.resolve(JSON.stringify(mockUser)));
+
+        const response = await request(app).get('/api/users/507f1f77bcf86cd799439011')
+
+        expect(response.status).to.equal(200)
+        expect(response.body).to.have.property('email', 'exemple@.gmail.com')
+    })
+
+    it('should return a 404 status if no user found', async () => {
+        
+        sinon.stub(UserModel, 'findById').returns(Promise.resolve(null))
+        sinon.stub(redisClient, 'get').returns(Promise.resolve(null));
+
+        const response = await request(app).get('/api/users/507f1f77bcf86cd799439011')
+
+        expect(response.status).to.equal(404)
+        expect(response.body).to.have.property('message', 'Not found')
+    })
+
+    it('should return a 500 status if there is a server error', async () => {
+        sinon.stub(UserModel, 'findById').throws(new Error('Server error'))
+        sinon.stub(redisClient, 'get').throws(new Error('Server error'))
+
+
+        const response = await request(app).get('/api/users/507f1f77bcf86cd799439011')
+
+        expect(response.status).to.equal(500)
+        expect(response.body).to.have.property('message', 'Server error')
+    })
+
+})
+
+describe('DELETE /api/users/:id', () => {
+    let app;
+
+    before(() => {
+        app = express();
+        app.use('/', routes);
+    });
+
+    afterEach(() => {
+        // Restore the original method after each test to avoid side effects
+        sinon.restore();
+    });
+
+    it('should return a 200 status if user was deleted', async () => {
+        const mockUser = { _id: '507f1f77bcf86cd799439011', email: 'exemple@.gmail.com', username: 'Asan', password: '123' };
+
+        sinon.stub(UserModel, 'findByIdAndDelete').returns(Promise.resolve(mockUser));
+    
+        const response = await request(app).delete('/api/users/507f1f77bcf86cd799439011');
+    
+        expect(response.status).to.equal(200);
+        expect(response.body).to.have.property('message', 'User was deleted' );
+    });
+
+    it('should return a 404 status if user wasn\'t defined', async () => {
+        sinon.stub(UserModel, 'findByIdAndDelete').returns(Promise.resolve(null))
+        const response = await request(app).delete('/api/users/507f1f77bcf86cd799439011');
+    
+        expect(response.status).to.equal(404);
+        expect(response.body).to.have.property('message', 'Not found' );
+    })
+
+    it('should return a 500 status if there is a server error', async () => {
+        sinon.stub(UserModel, 'findByIdAndDelete').throws(new Error('Database error'))
+        const response = await request(app).delete('/api/users/507f1f77bcf86cd799439011')
+
+        expect(response.status).to.equal(500)
+        expect(response.body).to.have.property('message', 'Database error')
+    })
+
+})
+
+
+
