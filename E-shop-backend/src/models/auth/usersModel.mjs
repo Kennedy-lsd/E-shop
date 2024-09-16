921908f6dd5449b21ab5077b072f4263aadd1738
Kennedy <santes1263@gmail.com>
2024-09-16 13:34:15 +0300

    Add indexing to database

--- E-shop-backend/src/models/auth/usersModel.mjs
+++ E-shop-backend/src/models/auth/usersModel.mjs
@@ -3,10 +3,12 @@ import { Schema } from "mongoose";
 
 
 const UserSchema = new Schema({
-    email: {type: String, required: true, unique: true},
-    username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true, index: true},
+    username: {type: String, required: true, unique: true, index: true},
     password: {type: String, required: true}
 })
+
+UserSchema.index({email: 1, username: 1})
 
 const UserModel = mongoose.model('UserModel', UserSchema)
 
--- E-shop-backend/src/models/products/productModel.mjs
+++ E-shop-backend/src/models/products/productModel.mjs
@@ -4,16 +4,17 @@ import { Schema } from "mongoose";
 
 const Product = new Schema({
     image: {type: String, required: true, unique: true},
-    title: {type: String, required: true, unique: true},
+    title: {type: String, required: true, unique: true, index: true},
     rating: {type: Number, required: true},
     colorway: {type: String, required: true},
     brand: {type: String, required: true},
     model: {type: String, required: true},
     releaseDate: {type: String, required: false},
-    department: {type: String, required: true},
+    department: {type: String, required: true, index: true},
     code: {type: String, required: true, unique: true}
 })
 
+Product.index({title: 1, department: 1})
 
 const ProductModel = mongoose.model('ProductModel', Product)
 
