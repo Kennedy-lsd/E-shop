9cda11a8bdff4927ec25a0f0cb434a35f27327a3
Kennedy <santes1263@gmail.com>
2024-09-17 23:37:02 +0300

    Create Footer Component

---
+++ E-shop-frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export function Footer() {
+    return (
+        <footer className="bg-dark text-white text-center py-3 mt-auto">
+        <div className="container">
+          &copy; {new Date().getFullYear()} ShopHome. All rights reserved.
+        </div>
+      </footer>
+    )
+}
